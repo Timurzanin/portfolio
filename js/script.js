dc3f083fd4f32844872a19c31424c320f03e3f5d
Timurzanin <95687372+Timurzanin@users.noreply.github.com>
2022-09-30 12:22:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -677,7 +677,7 @@ moda_overlay.addEventListener('click', () => {
 
 prev_btn.addEventListener('click', () => {
     if (currentIndex === 0) {
-        currentIndex = 5
+        currentIndex = 13
     } else {
         currentIndex--
     }
@@ -686,7 +686,7 @@ prev_btn.addEventListener('click', () => {
 });
 
 next_btn.addEventListener('click', () => {
-    if (currentIndex === 5) {
+    if (currentIndex === 13) {
         currentIndex = 0
     } else {
         currentIndex++
@@ -725,3 +725,27 @@ const swiper = new Swiper('.swiper', {
 
 
 //
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
